a39b4d655efb71104bd3fa86981583d60e2d1860
DeekshitaChigullapally <deekshitac@yahoo.com>
2014-10-05 10:31:27 -0700

    Changed CSS
    Opacity and Image added for first page. Firebase added for user input,
    but not correctly implemented.

--- listFunctions.js
+++ listFunctions.js
@@ -1,7 +1,10 @@
  //<script src="//code.jquery.com/jquery-1.10.2.js"></script>
+ var ref = new Firebase("https://smart-list.firebaseio.com/");
+
  function addItemToList(){
 	var listItem = document.getElementById('listItem').value;
 	var itemTime = document.getElementById('itemTime').value;
+	ref.push({item: listItem, time: itemTime});
 	document.getElementById('boldStuff2').innerHTML = listItem + " for " + itemTime + " days";
 	var node=document.createElement("LI");
 	var textnode=document.createTextNode(listItem);
@@ -10,7 +13,7 @@
 
 //	var x = document.createElement("INPUT");
  //    x.setAttribute("type", "checkbox");
-    document.getElementById("demo").appendChild(x);
+    // document.getElementById("demo").appendChild(x);
 
 	// var itemArray = new Array(2^32-1);
 	// var timeArray = new Array(2^32-1);
@@ -18,8 +21,28 @@
 	var timeArray = [];
 	itemArray.push(listItem);
 	itemArray.push(itemTime);
-
  }
+
+ //Firebase event listener
+ ref.on("child_added", function (snapshot) {
+ 	var item = snapshot.val();
+ 	console.log(item);
+ });
+
+ //Firebase User Authentiacation with email and password
+ ref.createUser({
+   var email = document.getElementBySelector('itemTime').value;	
+   email    : "bobtony@firebase.com",
+   password : "correcthorsebatterystaple"
+ }, function(error) {
+   if (error === null) {
+     console.log("User created successfully");
+   } else {
+     console.log("Error creating user:", error);
+   }
+ });
+
+
 
  	$(document).ready(function(){
   		$("li").click(function(){
